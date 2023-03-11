3ba59bfd94f31531722f50bf333529a3a6514729
Anastasiia Shulgina <anastasya.shulgina@gmail.com>
2023-03-12 00:14:14 +0100

    Task 11

---
+++ src/test/java/CheckCookieEx11.java
@@ -0,0 +1,22 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CheckCookieEx11 {
+
+    @Test
+    public void CheckCookie(){
+
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        //Map <String,String> cookieData = response.getCookies();
+        //System.out.println(cookieData);
+        assertEquals(response.cookie("HomeWork"), "hw_value");
+    }
+}
